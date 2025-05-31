61b1ee290b0b049c4315427080dd3d1f013357d0
Miray Hirabayashi <miraykgungor@gmail.com>
2025-05-30 23:13:13 -0500

    feat: implemented clear button

--- src/WordCharacterCounter.jsx
+++ src/WordCharacterCounter.jsx
@@ -9,11 +9,22 @@ function WordCharacterCounter() {
     setText(e.target.value);
   };
 
+  const handleClearText = () => {
+    setText("");
+  };
+
   return (
-    <div className="max-w-[800px] w-full p-2.5 h-[500px] mb-2.5 border border-gray-300 rounded-[5px] text-[25px] bg-white mt-10 mx-auto">
-     <div className='flex justify-between text-sm text-black pb-1'>
-        <span>Words: {wordCount}</span>
-        <span>Characters: {characterCount}</span>
+    <div className="max-w-[800px] w-full p-2.5 h-[505px] mb-2.5 border border-gray-300 rounded-[5px] text-[25px] bg-white mt-10 mx-auto">
+     <div className='flex justify-between items-center text-sm text-black pb-1'>
+        <div className="flex gap-4">
+          <span>Words: {wordCount}</span>
+          <span>Characters: {characterCount}</span>
+        </div>
+        <button
+      onClick={handleClearText}
+      className="px-4 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition"
+      >Clear
+      </button>
      </div>
 
      <div className='border-t border-gray-200 mb-2'></div>
